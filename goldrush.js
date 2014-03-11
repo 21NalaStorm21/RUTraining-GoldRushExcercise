ec98576d9f8fa7fe71d54041a02455342f33fafe
nala <nalastorm@gmail.com>
2014-03-11 11:53:41 -0600

    final

---
+++ goldrush.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+	$(document).on('click', '.container', function(e) {
+		e.preventDefault();
+		var goldSource = ($('<img src="goldImage.jpeg"></img>'));
+		goldSource.css('top', e.pageY);
+		goldSource.css('left', e.pageX);
+		goldSource.addClass('goldImageStyle');
+		$('.container').append($(goldSource));
+
+		var locationNotes = prompt("Would you like to enter notes about this location?");
+			console.log(locationNotes);
+			var locationNotesdiv = $('<div class="noteStyle">' + locationNotes + '</div>');
+			locationNotesdiv.css({
+				'top': e.pageY,
+				'left': e.pageX,
+				'background-color' : '#CFB53B',
+				'font-family' : 'Times New Roman',
+				'border-radius' : '5px',
+				'font-size' : '18px'
+			});
+
+
+		goldSource.on('mouseenter', function() {
+			e.preventDefault();
+			console.log(locationNotes);
+			$('.container').append(locationNotesdiv);
+			locationNotesdiv.show();
+
+		goldSource.on('mouseleave', function(){
+			e.preventDefault();
+			locationNotesdiv.hide();
+		})
+
+	});
+
+	});
+
+
+	$(document).on('click','.goldImageStyle', function(e) {
+		e.preventDefault();
+		$(this).hide();
+		e.stopPropagation();
+
+	});
+
+
+});
